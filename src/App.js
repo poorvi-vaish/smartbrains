503611616a11dd2f3423e02bc883d5fb563a5c75
Poorvi Vaish <img_2019047@iiitm.ac.in>
2021-03-22 23:26:52 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import 'tachyons';
+function App() {
+  return (
+    <div className="App">
+     <Navigation /> 
+     <Logo />
+    </div>
+  );
+}
+
+export default App;
